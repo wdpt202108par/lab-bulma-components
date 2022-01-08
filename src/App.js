f3d0f4f9fe997134c79dd1719e4a0b8f5c69fd30
Lea Beaulieu <beaulieu.lea@free.fr>
2022-01-08 15:59:58 +0100

    done iteration 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import FormField from './formfield/FormField.js';
 import Navbar from './navbar/Navbar.js';
 
 const App = () => {
-  return  <div className="App">
-          <Navbar></Navbar>
-          </div>;
+  return  (
+    <div className="App">
+        <Navbar />
+        <FormField label="Firstname" type="text" placeholder="John" />
+        <FormField label="Couriel" type="email" placeholder="jdoe@gmail.com" />
+    </div>
+  );
 };
 
 export default App;
