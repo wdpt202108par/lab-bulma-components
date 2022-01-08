ac63b19ea78b2b5158cd9e9f4cc2ea96ecc1ce10
hassan sabeh <h.adamsabeh@yahoo.com>
2022-01-08 16:47:24 +0100

    iteration 3 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Navbar from './navbar/Navbar.js';
+import FormField from './formfield/FormField.js';
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return (
+  <>
+      <Navbar />
+      <FormField label="Name" placeholder="ex. Julie" type="text"/>
+      <FormField label="email" placeholder="ex. julie@ironhack.com" type="email"/>
+  </>
+  );
 };
 
 export default App;
