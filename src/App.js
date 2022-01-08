9cd5f3227318f823f92df0b37e1ce6f48ad54e44
Yooyoo56 <tak.yoojeong@gmail.com>
2022-01-08 14:20:55 +0100

    iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './App.css'
+
+// import Bulma CSS
+import 'bulma/css/bulma.css';
+
 
 const App = () => {
-  return <h1>Bulma</h1>;
+  return <div id = "root"></div>;
 };
 
 export default App;
