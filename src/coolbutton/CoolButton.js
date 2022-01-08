b0b8b0045648c64e4083f3d1fceeee870e97aa79
Yooyoo56 <tak.yoojeong@gmail.com>
2022-01-08 16:59:37 +0100

    iteration 3 done

--- src/coolbutton/CoolButton.js
+++ src/coolbutton/CoolButton.js
@@ -3,6 +3,12 @@ import "bulma/css/bulma.css";
 
 function CoolButton(props) {
 	let klass = `button ${props.className}`; // "is-rounded my-class"
+	if (props.isDanger) {
+		klass += "is-danger";
+	}
+	if (props.isSuccess) {
+		klass += " is-success";
+	}
 
 	// si on a recu la prop isSmall => rajouter la classe 'is-small' a `klass`
 	// si on a recu la prop isDanger => rajouter la classe 'is-danger' a `klass`
